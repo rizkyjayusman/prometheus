e7a1752b1655b6ea42f25a05eba4579c028ef6e2
Rizky Jayusman <rizky.jayusman96@gmail.com>
2024-02-12 16:58:27 +0700

    feat: alert using webhook

---
+++ actuator/src/main/java/com/rizkyjayusman/actuator/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.rizkyjayusman.actuator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
--- actuator/src/main/java/com/rizkyjayusman/actuator/controller/HomeController.java
+++ actuator/src/main/java/com/rizkyjayusman/actuator/controller/HomeController.java
@@ -1,12 +1,35 @@
 package com.rizkyjayusman.actuator.controller;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("houseController")
+import java.util.Map;
+
+@RestController
 public class HomeController {
 
-    @GetMapping("/home")
-    public void home() {}
+    @Autowired
+    private MeterRegistry meterRegistry;
+
+    @GetMapping("/2xx")
+    public String simulate2xxResponse() {
+        meterRegistry.counter("orders.2xx","status","OK").increment();
+        return "Got 2xx Response";
+    }
+
+    @GetMapping("/5xx")
+    public String simulate5xxResponse() {
+        meterRegistry.counter("orders.5xx","status","NOTOK").increment();
+        return "Got 5xx Response";
+    }
+
+    @PostMapping("/alert-hook")
+    public void receiveAlertHook(@RequestBody Map request) throws Exception {
+        System.out.println(request);
+    }
 
 }
